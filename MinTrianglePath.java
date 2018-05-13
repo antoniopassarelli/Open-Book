ae18c4152b9be32e0c22abadbe536fbe5e12cdc2
Antonio <antonio@localhost.localdomain>
2018-05-13 16:24:58 +0200

    merged into one class

--- MinTrianglePath.java
+++ MinTrianglePath.java
@@ -44,18 +44,50 @@ public class MinTrianglePath {
 				triangle[i][j] = triangleAux.get(i).get(j);
 			}
 		}
-		Triangle t = new Triangle(triangle);
-		TrianglePath result = t.findShortestPath();
-		int[] solution = (result.getPath());
+
+		// calculate solution
+		int[] solution = findShortestPath(triangle);
 		System.out.println("Shortest path:");
 		for (int i = 0; i < solution.length; i++) {
-			int n = t.getTriangle()[i][solution[i]];
+			int n = triangle[i][solution[i]];
 			System.out.print(n);
 			if (i < solution.length - 1) {
 				System.out.print(" -> ");
 			}
 		}
 		System.out.println();
+	}
+
+	public static int[] findShortestPath(int[][] triangle) {
+		// public TrianglePath findShortestPath(int[][] triangle) {
+		int[][] aux = copy2Array(triangle);
+		int[][] vectors = new int[aux.length][];
+		// iterate rows backward
+		for (int row = aux.length - 2; row >= 0; row--) {
+			vectors[row] = new int[aux[row].length];
+			// iterate columns backward
+			for (int col = aux[row].length - 2; col >= 0; col--) {
+				int min;
+				min = Math.min(aux[row + 1][col], aux[row + 1][col + 1]);
+				aux[row][col] += min;
+				// the value in vectors will be the place/column the value came from
+				vectors[row][col] = aux[row + 1][col] == min ? col : col + 1;
+			}
+		}
+		int[] path = new int[vectors.length];
+		for (int i = 1; i < vectors.length; i++) {
+			path[i] = vectors[i - 1][path[i - 1]];
+		}
+		// return new TrianglePath(path);
+		return path;
+	}
+
+	static int[][] copy2Array(int[][] input) {
+		int[][] result = new int[input.length][];
+		for (int i = 0; i < input.length; i++) {
+			result[i] = Arrays.copyOf(input[i], input[i].length);
+		}
+		return result;
 	}
 
 }
